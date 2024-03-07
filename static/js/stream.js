d33903c4002f287f742c62d8f5ae7268b3c4c86d
MuhammadUsman15 <80236188+MuhammadUsman15@users.noreply.github.com>
2024-03-07 22:13:31 +0500

    Agora SDK Integrate

---
+++ static/js/stream.js
@@ -0,0 +1,25 @@
+const APP_ID = 'Your API'
+const CHANNEL = 'main'
+const TOKEN = 'Your Token'
+let UID;
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+    let player = `<div  class="video-container" id="user-container-${UID}">
+                     <div class="video-player" id="user-${UID}"></div>
+                     <div class="username-wrapper"><span class="user-name"></span></div>
+                  </div>`
+    
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    localTracks[1].play(`user-${UID}`)
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+joinAndDisplayLocalStream()
